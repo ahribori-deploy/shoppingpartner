b4124bf9dd73a2c35f2b8d418fdcfc6bf3ff992f
Daniel <ahribori@gmail.com>
2022-02-15 15:52:24 +0900

    first commit

---
+++ gatsby-meta-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteUrl: `https://shoppingpartner.kr`,
+  name: '쇼핑파트너',
+  shortDescription: '쇼핑파트너',
+  description: '합리적인 소비를 위해 좋은 상품을 추천해주는 쇼핑파트너입니다.',
+  author: '쇼핑파트너',
+  ga: '299045888', // Google Analytics Tracking ID
+};
